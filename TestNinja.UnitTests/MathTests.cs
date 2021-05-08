78297711ff16d3c3ce3cfdebf10ecf926bd0fffa
dariuszadamczyk <darius.adamczyk@outlook.com>
2021-05-08 15:36:14 +0200

    Parameterized Tests

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -42,5 +42,15 @@ namespace TestNinja.UnitTests
             Assert.That(result, Is.EqualTo(1));
         }
         
+        [Test]
+        [TestCase(2,1,2)]
+        [TestCase(1,2,2)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+        
     }
 }
