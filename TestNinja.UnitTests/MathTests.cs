7bdf840f874d1cf848b9763a4eca63c8eef14d00
dariuszadamczyk <darius.adamczyk@outlook.com>
2021-09-12 22:20:44 +0200

    Testing Arrays

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -52,6 +53,26 @@ namespace TestNinja.UnitTests
             var result = _math.Max(a, b);
             Assert.That(result, Is.EqualTo(expectedResult));
         }
-        
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThenZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+            
+            //Assert.That(result, Is.Not.Empty);
+            
+            //Assert.That(result.Count(), Is.EqualTo(3));
+            
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+            
+            Assert.That(result, Is.EquivalentTo(new [] {1,3,5}));
+            
+            Assert.That(result, Is.Ordered); //Make sure result is ordered
+            Assert.That(result, Is.Unique); //Make sure result is unique
+
+            
+        }
     }
 }
